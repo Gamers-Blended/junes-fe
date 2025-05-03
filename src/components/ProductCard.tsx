21b73ce7cf153afe58de46f0082c87cf3610ac23
AB <wong_zhihang@u.nus.edu>
2025-05-03 17:31:41 +0800

    [MFLP-24] add trimming of title + styling of product info

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,14 +10,19 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ item, isLoading }) => {
+    const wordLimit = 40;
+    // Title will only show up to wordLimit characters
+    const trimmedTitle = item.title.length > wordLimit ? `${item.title.substring(0, wordLimit)}...` : item.title;
 
-  return (
-            <div className={`product-slider-card ${isLoading ? 'disabled' : ''}`}>
-              <img className='product-slider-image' src={item.imageSrc} alt={item.title} />
-              <h3>{item.title}</h3>
-              <p>{item.price}</p>
+    return (
+            <div className={`product-card-container ${isLoading ? 'disabled' : ''}`}>
+                <div className='product-card-image-container'>
+                    <img className='product-card-image' src={item.imageSrc} alt={item.title} />
+                </div>
+                <p className='product-card-title'>{trimmedTitle}</p>
+                <p className='product-card-price'>{item.price}</p>
             </div>
-  );
+    );
 };
 
 export default ProductCard;
