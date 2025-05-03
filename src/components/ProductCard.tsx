4ba8fbf455799d34cd9bc70da431720828d8d906
AB <wong_zhihang@u.nus.edu>
2025-05-03 19:51:40 +0800

    [MFLP-24] add buttons when hovering over card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -14,14 +14,34 @@ const ProductCard: React.FC<ProductCardProps> = ({ item, isLoading }) => {
     // Title will only show up to wordLimit characters
     const trimmedTitle = item.title.length > wordLimit ? `${item.title.substring(0, wordLimit)}...` : item.title;
 
+    const handleQuickShop: Function = (title:string) => {
+        console.log(`Quick shop for ${title}`);
+    }
+
+    const handleAddToCart: Function = (title:string) => {
+        console.log(`Add to cart for ${title}`);
+    }
+
     return (
-            <div className={`product-card-container ${isLoading ? 'disabled' : ''}`}>
-                <div className='product-card-image-container'>
-                    <img className='product-card-image' src={item.imageSrc} alt={item.title} />
-                </div>
-                <p className='product-card-title'>{trimmedTitle}</p>
-                <p className='product-card-price'>{item.price}</p>
+        <div className={`product-card-container ${isLoading ? 'disabled' : ''}`}>
+            <div className='product-card-image-container'>
+                <img className='product-card-image' src={item.imageSrc} alt={item.title} />
             </div>
+            <p className='product-card-title'>{trimmedTitle}</p>
+            <p className='product-card-price'>{item.price}</p>
+
+            {/* Buttons that appear on hover */}
+            <div className='product-card-buttons'>
+                <button className="product-card-button" onClick={handleQuickShop(item.title)}>
+                    Quick Shop
+                </button>
+                <button className="product-card-button add-to-cart-button" onClick={handleAddToCart(item.title)}>
+                    Add To Cart
+                </button>
+            </div>
+
+                
+        </div>
     );
 };
 
