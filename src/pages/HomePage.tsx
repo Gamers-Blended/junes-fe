8ba00c3a196b7f44c25b4c79a5379d6a45a76146
gamers-blended <qwertygp@proton.me>
2025-06-28 17:45:39 +0800

    [MFLP-49] feat: replace hardcoded recommended product list with one called from BE API

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,138 +1,91 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import PromoCarousel from "../components/PromoCarousel";
 import ProductSlider from "../components/ProductSlider";
-import ApolloJusticeAceAttorneyTrilogyUSNSW from "../assets/products/nsw_us_apollo_justice_ace_attorney_trilogy.jpg";
-import AtelierMarieRemakeASIANSW from "../assets/products/nsw_asia_atelier_marie_remake.jpg";
-import BatenKaitosRemasterASIANSW from "../assets/products/nsw_asia_baten_kaitos_i_ii_hd_remaster.jpg";
-import ChronoCrossTrdeASIANSW from "../assets/products/nsw_asia_chrono_cross_the_radical_dreamers_edition.jpg";
-import FatalFameMobwASIANSW from "../assets/products/nsw_asia_fatal_frame_maiden_of_black_water.jpg";
-import FatalFameMobwASIAPS4 from "../assets/products/ps4_asia_fatal_frame_maiden_of_black_water.jpg";
-import FatalFameMotleASIANSW from "../assets/products/nsw_asia_fatal_frame_mask_of_the_lunar_eclipse.jpg";
-import FatalFameMotleASIAPS4 from "../assets/products/ps4_asia_fatal_frame_mask_of_the_lunar_eclipse.jpg";
-import FinalFantasyPixelRemasterASIANSW from "../assets/products/nsw_asia_final_fantasy_pixel_remaster_collection.jpg";
-import FinalFantasyXIASIANSW from "../assets/products/nsw_asia_final_fantasy_ix.jpg";
-import FinalFantasyVIIVIIITwinEURNSW from "../assets/products/nsw_eur_final_fantasy_vii_final_fantasy_viii_remastered_twin_pack.jpg";
-import GrandiaASIANSW from "../assets/products/nsw_asia_grandia_hd_collection.jpg";
-import AceAttorneyTrilogyJPNSW from "../assets/products/nsw_jp_phoenix_wright_ace_attorney_trilog.jpg";
-import NinjaGaidenCollectionASIANSW from "../assets/products/nsw_asia_ninja_gaiden_master_collection.jpg";
-import NinjaGaidenCollectionASIAPS4 from "../assets/products/ps4_asia_ninja_gaiden_master_collection.jpg";
-import SagaFrontierASIANSW from "../assets/products/nsw_asia_saga_frontier_remastered.jpg";
+import { useAuth } from "../components/AuthContext.tsx";
+
+
+interface ProductSliderItem {
+  id: string;
+  name: string;
+  slug: string;
+  price: string;
+  productImageUrl: string;
+}
 
 function HomePage() {
-  // Recommended items to user
-  const recommendedItems = [
-    {
-      id: "1",
-      name: "Apollo Justice Ace Attorney Trilogy",
-      slug: "test",
-      price: "S$40.00",
-      productImageUrl: ApolloJusticeAceAttorneyTrilogyUSNSW,
-    },
-    {
-      id: "2",
-      name: "Atelier Marie Remake: The Alchemist of Salburg",
-      slug: "test",
-      price: "S$70.00",
-      productImageUrl: AtelierMarieRemakeASIANSW,
-    },
-    {
-      id: "3",
-      name: "Baten Kaitos I & II HD Remaster",
-      slug: "test",
-      price: "S$60.00",
-      productImageUrl: BatenKaitosRemasterASIANSW,
-    },
-    {
-      id: "4",
-      name: "Chrono Cross: The Radical Dreamers Edition",
-      slug: "test",
-      price: "S$70.00",
-      productImageUrl: ChronoCrossTrdeASIANSW,
-    },
-    {
-      id: "5",
-      name: "Fatal Frame: Mask of the Lunar Eclipse",
-      slug: "test",
-      price: "S$70.00",
-      productImageUrl: FatalFameMotleASIANSW,
-    },
-    {
-      id: "6",
-      name: "Fatal Frame: Mask of the Lunar Eclipse",
-      slug: "test",
-      price: "S$70.00",
-      productImageUrl: FatalFameMotleASIAPS4,
-    },
-    {
-      id: "7",
-      name: "Fatal Frame: Maiden of Black Water",
-      slug: "test",
-      price: "S$60.00",
-      productImageUrl: FatalFameMobwASIANSW,
-    },
-    {
-      id: "8",
-      name: "Fatal Frame: Maiden of Black Water",
-      slug: "test",
-      price: "S$60.00",
-      productImageUrl: FatalFameMobwASIAPS4,
-    },
-    {
-      id: "9",
-      name: "Final Fantasy Pixel Remaster Collection",
-      slug: "test",
-      price: "S$70.00",
-      productImageUrl: FinalFantasyPixelRemasterASIANSW,
-    },
-    {
-      id: "10",
-      name: "Final Fantasy XI",
-      slug: "test",
-      price: "S$40.00",
-      productImageUrl: FinalFantasyXIASIANSW,
-    },
-    {
-      id: "11",
-      name: "Final Fantasy VII Final Fantasy VIII Remastered Twin Pack",
-      slug: "test",
-      price: "S$40.00",
-      productImageUrl: FinalFantasyVIIVIIITwinEURNSW,
-    },
-    {
-      id: "12",
-      name: "Grandia HD Collection",
-      slug: "test",
-      price: "S$50.00",
-      productImageUrl: GrandiaASIANSW,
-    },
-    {
-      id: "13",
-      name: "Ace Attorney Trilogy",
-      slug: "test",
-      price: "S$40.00",
-      productImageUrl: AceAttorneyTrilogyJPNSW,
-    },
-    {
-      id: "14",
-      name: "Ninja Gaiden Master Collection",
-      slug: "test",
-      price: "S$40.00",
-      productImageUrl: NinjaGaidenCollectionASIANSW,
-    },
-    {
-      id: "15",
-      name: "Ninja Gaiden Master Collection",
-      slug: "test",
-      price: "S$40.00",
-      productImageUrl: NinjaGaidenCollectionASIAPS4,
-    },
-    {
-      id: "16",
-      name: "Saga Frontier Remastered",
-      slug: "test",
-      price: "S$40.00",
-      productImageUrl: SagaFrontierASIANSW,
-    },
-  ];
+  const [recommendedItems, setRecommendedItems] = useState<ProductSliderItem[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const { userID } = useAuth();
+  const pageNumber = 0;
+
+  useEffect(() => {
+    const api = axios.create({
+        baseURL: 'http://localhost:8080/junes/api/v1',
+        timeout: 10000,
+    });
+  
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        console.info("Fetching data for recommended products...")
+
+        const response = await api.get<ProductSliderItem[]>(`/frontpage/recommended?userID=${userID}&pageNumber=${pageNumber}`);
+        const urlPrefix = "https://pub-6e933b871f074c2c83657430de8cf735.r2.dev/";
+
+        // Append prefix to each productImageUrl
+        const updatedData = response.data.map((item) => ({
+          ...item,
+          productImageUrl: item.productImageUrl ? `${urlPrefix}${item.productImageUrl}` : "",
+        }));
+
+        setRecommendedItems(updatedData);
+        console.log("Fetched data: {}", recommendedItems)
+        // let response;
+
+        // if (isLoggedIn) {
+        //   response = await api.get<ProductSliderItem[]>(`/frontpage/recommended?userID=${userID}&pageNumber=${pageNumber}`);
+        // } else {
+        //   const requestData = {
+        //     pageNumber: pageNumber,
+        //     historyCache: ["item1"]
+        //   };
+        //   response = await api.post<ProductSliderItem[]>(`/frontpage/recommended/no-user`, requestData, {headers: {
+        //       'Content-Type': 'application/json'
+        //     }});
+        // }
+
+      } catch (error) {
+        console.error('Error fetching data for recommended products (user logged in): ', error);
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+  }, []); // Runs once component mounts
+  
+  if (loading) {
+    return (
+      <div className="home-page-container">
+        <PromoCarousel />
+        <div>Loading recommended items...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="home-page-container">
+        <PromoCarousel />
+        <div>Error loading recommended items: {error}</div>
+      </div>
+    );
+  }
+  
 
   return (
     <div className="home-page-container">
