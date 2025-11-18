f8e48544b5fcdf03055cd9181e76eb20b1a7cfe3
Gamers-Blended <zhihangwong@proton.me>
2025-11-19 07:56:52 +0800

    [MFLP-125] feat: add shipping details

--- src/pages/OrderDetailsPage.tsx
+++ src/pages/OrderDetailsPage.tsx
@@ -44,6 +44,34 @@ const OrderDetailsPage: React.FC = () => {
 
       <OrderTable orderData={mockOrderList[0]} mode="details" />
 
+      {/* Shipping Details */}
+      <div className="order-footer-info">
+        <div className="shipping-address-section">
+          <h3>Shipping Address</h3>
+          <div>{mockOrderList[0].shippingAddress?.addressLine}</div>
+          <div>{mockOrderList[0].shippingAddress?.unitNumber}</div>
+          <div>{mockOrderList[0].shippingAddress?.country}</div>
+          <div>{mockOrderList[0].shippingAddress?.zipCode}</div>
+          <div>{mockOrderList[0].shippingAddress?.phoneNumber}</div>
+        </div>
+
+        <div className="shipping-info-section">
+          <h3>Shipping</h3>
+          <div className="shipping-info">
+            <div className="shipping-info-labels">
+              <div>Shipping Weight:</div>
+              <div>Tracking Number:</div>
+              <div>Shipping Date:</div>
+            </div>
+            <div className="shipping-info-values">
+              <div>{mockOrderList[0].shippingWeight}</div>
+              <div>{mockOrderList[0].trackingNumber}</div>
+              <div>{mockOrderList[0].shippedDate}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
