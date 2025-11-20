d742a27df34e17e78f6e4d0711a3f484eaf88771
Gamers-Blended <zhihangwong@proton.me>
2025-11-21 05:18:29 +0800

    [MFLP-145] feat: remove navigation bar for invoice page

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,72 @@
-import './App.css';
-import HomePage from './pages/HomePage.tsx';
-import LoginPage from './pages/LoginPage.tsx';
-import CreateNewUserPage from './pages/CreateNewUserPage.tsx';
-import ForgetPasswordPage from './pages/ForgetPasswordPage.tsx';
-import EmailSentPage from './pages/EmailSentPage.tsx';
-import MyAccountPage from './pages/MyAccountPage.tsx';
-import ChangeCredentialsPage from './pages/ChangeCredentialsPage.tsx';
-import SavedInfoPage from './pages/SavedInfoPage.tsx';
-import ModifyAddressPage from './pages/ModifyAddressPage.tsx';
-import ProductsPage from './pages/ProductsPage.tsx';
-import ProductListingPage from './pages/ProductListingPage.tsx';
-import ProductDetailsPage from './pages/ProductDetailsPage.tsx';
-import OrderDetailsPage from './pages/OrderDetailsPage.tsx';
-import InvoicePage from './pages/InvoicePage.tsx';
-import CartPage from './pages/CartPage.tsx';
-import WishListPage from './pages/WishListPage.tsx';
+import "./App.css";
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import CreateNewUserPage from "./pages/CreateNewUserPage.tsx";
+import ForgetPasswordPage from "./pages/ForgetPasswordPage.tsx";
+import EmailSentPage from "./pages/EmailSentPage.tsx";
+import MyAccountPage from "./pages/MyAccountPage.tsx";
+import ChangeCredentialsPage from "./pages/ChangeCredentialsPage.tsx";
+import SavedInfoPage from "./pages/SavedInfoPage.tsx";
+import ModifyAddressPage from "./pages/ModifyAddressPage.tsx";
+import ProductsPage from "./pages/ProductsPage.tsx";
+import ProductListingPage from "./pages/ProductListingPage.tsx";
+import ProductDetailsPage from "./pages/ProductDetailsPage.tsx";
+import OrderDetailsPage from "./pages/OrderDetailsPage.tsx";
+import InvoicePage from "./pages/InvoicePage.tsx";
+import CartPage from "./pages/CartPage.tsx";
+import WishListPage from "./pages/WishListPage.tsx";
 import NavigationBar from "./components/NavigationBar.tsx";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AuthProvider } from "./components/AuthContext";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <AuthProvider>
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products/:id" element={<ProductsPage />} />
-        <Route path="/products/listings/:platform" element={<ProductListingPage />} />
-        <Route path="/order/:orderId" element={<OrderDetailsPage />} />
-        <Route path="/invoice/:orderId" element={<InvoicePage />} />
-        <Route path="/games/:slug" element={<ProductDetailsPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/createaccount" element={<CreateNewUserPage />} />
-        <Route path="/resetpassword" element={<ForgetPasswordPage />} />
-        <Route path="/emailsent" element={<EmailSentPage />} />
-        <Route path="/myaccount" element={<MyAccountPage />} />
-        <Route path="/changecredentials" element={<ChangeCredentialsPage />} />
-        <Route path="/savedinfo" element={<SavedInfoPage />} />
-        <Route path="/modifyaddress" element={<ModifyAddressPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/wishlist" element={<WishListPage />} />
-      </Routes>
-    </Provider>
-    <NavigationBar/>
-    </AuthProvider>
+      <AuthProvider>
+        <AppLayout />
+      </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+function AppLayout() {
+  const location = useLocation();
+  const hideNavBar = location.pathname.startsWith("/invoice/");
+
+  return (
+    <>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/:id" element={<ProductsPage />} />
+          <Route
+            path="/products/listings/:platform"
+            element={<ProductListingPage />}
+          />
+          <Route path="/order/:orderId" element={<OrderDetailsPage />} />
+          <Route path="/invoice/:orderId" element={<InvoicePage />} />
+          <Route path="/games/:slug" element={<ProductDetailsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/createaccount" element={<CreateNewUserPage />} />
+          <Route path="/resetpassword" element={<ForgetPasswordPage />} />
+          <Route path="/emailsent" element={<EmailSentPage />} />
+          <Route path="/myaccount" element={<MyAccountPage />} />
+          <Route
+            path="/changecredentials"
+            element={<ChangeCredentialsPage />}
+          />
+          <Route path="/savedinfo" element={<SavedInfoPage />} />
+          <Route path="/modifyaddress" element={<ModifyAddressPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/wishlist" element={<WishListPage />} />
+        </Routes>
+      </Provider>
+
+      {!hideNavBar && <NavigationBar />}
+    </>
+  );
+}
+
+export default App;
