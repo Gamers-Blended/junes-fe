c545b826a8f396e42e39aa1683ce9ff23fd2438e
Gamers-Blended <zhihangwong@proton.me>
2025-11-22 04:29:08 +0800

    [MFLP-14] feat: add top portion of Shopping Cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,7 +1,22 @@
+import checkoutIcon from "../assets/checkoutIcon.png";
+
 const CartPage = () => {
   return (
-    <div>
-      <h1>Cart Page</h1>
+    <div className="cart-page-container">
+      <div className="common-header">
+        <h1>MY CART</h1>
+      </div>
+
+      <div className="sub-header">
+        <div className="sub-header-subtotal">
+          <div className="subtotal-label">Subtotal</div>
+          <span className="subtotal-amount">$299.97</span>
+        </div>
+        <button className="form-button checkout-button">
+          <img src={checkoutIcon} alt="Checkout" className="checkout-icon" />
+          Checkout
+        </button>
+      </div>
     </div>
   );
 };
