11e397547f8c43b396042eecbe10efc5472841f0
Gamers-Blended <zhihangwong@proton.me>
2025-12-01 01:58:35 +0800

    [MFLP-150] feat: add links to Order Placed page

--- src/pages/OrderPlacedPage.tsx
+++ src/pages/OrderPlacedPage.tsx
@@ -1,7 +1,22 @@
 import { mockUserData } from "../mocks/data/userData";
+import { mockOrderList } from "../mocks/data/orders";
+import { useNavigate } from "react-router-dom";
 
 const OrderPlacedPage = () => {
-  const orderId = "J1234";
+  const orderId = mockOrderList[0].id;
+  const navigate = useNavigate();
+
+  const handleViewOrderDetails = (orderId: string) => {
+    const url = `/order/${orderId}`;
+    console.log(`Navigating to order details for ${orderId}`);
+    navigate(url);
+  };
+
+  const handleContinueShopping = (): void => {
+    console.log("Continuing shopping");
+    navigate("/");
+  };
+
   return (
     <div className="form-parent-container">
       <div className="form">
@@ -16,6 +31,20 @@ const OrderPlacedPage = () => {
           </p>
           <div className="order-id-text">#{orderId}</div>
         </div>
+
+        <div className="spacer"></div>
+
+        <button
+          className="action-link"
+          onClick={() => handleViewOrderDetails(orderId)}
+        >
+          View my order
+        </button>
+        <br />
+
+        <button className="action-link" onClick={handleContinueShopping}>
+          Continue shopping &gt;
+        </button>
       </div>
     </div>
   );
