9750394fa19f04c19f7071cd432c7469eb98c1e6
Gamers-Blended <zhihangwong@proton.me>
2025-11-30 23:29:56 +0800

    [MFLP-149] feat: add header and text to Order Placed page

--- src/pages/OrderPlacedPage.tsx
+++ src/pages/OrderPlacedPage.tsx
@@ -1,5 +1,24 @@
+import { mockUserData } from "../mocks/data/userData";
+
 const OrderPlacedPage = () => {
-  return <h1>WE HAVE RECEIVED YOUR ORDER!</h1>;
+  const orderId = "J1234";
+  return (
+    <div className="form-parent-container">
+      <div className="form">
+        <div className="form-title">
+          <h1>WE HAVE RECEIVED YOUR ORDER!</h1>
+        </div>
+
+        <div className="form-container">
+          <p className="form-text text-align-center">
+            A confirmation email has been sent to {mockUserData.email}.<br />
+            Your order ID is
+          </p>
+          <div className="order-id-text">#{orderId}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderPlacedPage;
